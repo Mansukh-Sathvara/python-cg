39fd6b69bfe30f2243a75d878facf198971b84c3
Mansukh_Sathvara <mansukh.sathvara.cg@gmail.com>
2026-09-04 19:25:35 +0530

    input and output added

---
+++ 07.input_and_output/index.py
@@ -0,0 +1,115 @@
+a=input("Enter your name:")
+print(a)
+
+a=input("Enter your city:")
+print(a)
+
+a=input("Enter your user name:")
+b=input("age:")
+print(a,b)
+
+a=input("Mansukh")
+print(type(a))
+
+a=input("Enter you value :")
+print("Enter a type:",type(a))
+
+a=input("My first name :")
+b=input("My last name :")
+
+a=input("My name :")
+b=input("My city name :")
+b=input("My college name :")
+
+a="Mansukh"
+b="Lacky"
+print(f"My first name is {a} and my last name is {b}.")
+
+a,b=input("enter any statement:").split()
+print(f"first name is {a} last name is {b}")
+
+a,b,c=input("Enter your three number :").split()
+print(a)
+print(b)
+print(c)
+
+a="25"
+print(int(a))
+
+a="25.5"
+print(float(a))
+
+a=input("enter your num :")
+print(type(a))
+
+a=int(input("Enter a number :"))
+print(type(a))
+
+a=float(input("Enter you number :"))
+print(type(a))
+
+16
+a=input("first num :")
+b=input("second num :")
+print(a + b)
+
+17
+a = int(input("first num :"))
+b = int(input("second num :"))
+print(a + b)
+
+name = "Rahul"
+age = 20
+print(f"my name is {name} and i am {age} years old")
+
+
+a = 10
+b = 20
+print(f"sum = {a+b}")
+
+a,b=input("enter age and name:").split()
+print(f"my name is {b} and my age is {a}")
+
+c,d=input("enter price of both:").split()
+c=float(c)
+d=float(d)
+print(f"first price {c:.2f} last price {d:.2f}")
+print(type(c),type(d))
+
+
+
+# print(c:.2f,b:.2f)   dout
+
+a,b,c=input("enter product name price quantity").split()
+print(f"product name {a} price {b} quantity{c}")
+
+print("a", "b", "c")
+
+print("2026", "08", "19", sep="-")
+
+print("hello", end=" ")
+print("world")
+
+a,b=input("enter first and last number:").split()
+a=int(a)
+b=int(b)
+print(f"sum {a + b}")
+
+
+a=input("price:")
+b=input("product quantity")
+a=int(a)
+b=int(b)
+print(f"total={a*b}")
+
+
+name=input("enter name:")
+age=int(input("enter age:"))
+marks=int(input("enter marks:"))
+print(f"my name is {name} and age is {age} and marks is {marks}")
+
+a=input("student name :")
+b=int(input("student age :"))
+c=float(input("student height :"))
+d=input("enter your city :")
+print(f"my name is {a},age {b},height is {c:.2f},city {d}")
