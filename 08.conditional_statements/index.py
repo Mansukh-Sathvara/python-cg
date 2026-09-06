b05e1bf89ff10c2726ceb93fb053cde52e10bc01
Mansukh_Sathvara <mansukh.sathvara.cg@gmail.com>
2026-09-06 23:55:05 +0530

    08.conditional_statements folder add.

---
+++ 08.conditional_statements/index.py
@@ -0,0 +1,67 @@
+a=int(input("Enter your Number :"))
+
+if a>10:
+    print("your number is corect :")
+
+q2
+a=int(input("Enter your age :"))
+
+if a>18:
+    print("Your are a Adult")
+
+q3
+num=float(input("Enter your number :"))
+if num>0:
+    print("Positive")
+
+q4
+marks=float(input("Enter your marks :"))
+if marks>=40:
+    print("your are pass.")
+
+q5
+a=int(input("Enter your your number :"))
+
+if a==0:
+    print("zero")
+
+q6
+num=int(input("enter your number :"))
+if num>0:
+    print("positive :")
+
+else:
+    print("Not positive :")       
+
+q7
+age=int(input("Enter your age :"))
+if age>=18:
+    print("Your are Adult :")
+
+else:
+    print("your are Minor :")    
+
+q8
+num=int(input("enter your number :"))
+if num%2==0:
+    print("even number :")
+
+else:
+    print("odd number :")
+
+q9
+marks=float(input("Enter your marks :"))
+if marks>=40:
+    print("Your are a pass :")
+
+else:
+    print("Your are a fail :")    
+
+q10
+num1=float(input("enter your first number :"))
+num2=float(input("enter your second number :"))
+if num1>=num2:
+    print("First number is greater :")
+else :
+  
+    print("Second number is not greater :")
