a0f45f4577d96e577cf4214aad647d029bb7ea76
Mansukh_Sathvara <mansukh.sathvara.cg@gmail.com>
2026-09-10 09:37:17 +0530

    for loop.py file add.

---
+++ 10.For_loop/index.py
@@ -0,0 +1,81 @@
+#q1
+for a in range(5):
+    print("hello")
+
+#q2
+for a in range(10):
+    print(a)
+
+#q3
+for i in range(11):
+    print(i)
+
+#q4
+for i in range(10,0,-1):
+    print(i)
+
+#q5
+for i in range(5,50,5):
+    print(i)
+
+#q6
+for i in range(2,20):
+    if i%2==0:
+        print(i)
+
+    #q7
+for i in range(1,19):
+    if i%2==1:
+        print(i) 
+
+#q8
+for i in range(2,20):
+ if i%3==0:
+    print(i)
+    
+ex:-
+for i in range(5,51,5):
+    print(i)
+
+#q9
+for i in range(20,1,-1):
+    print(i)
+
+#q10
+n=int(input("Enter your number :"))
+for i in range(1,n+1):
+    print(i)
+
+q11
+n=int(input("Enter your number:"))
+for i in range(1,n+1):
+   if  i%2==0:
+      print(i)
+
+#q12
+n=int(input("Enter your number:"))
+for i in range(1,n+1):
+   if  i%2==1:
+      print(i)
+       
+#q13
+n=int(input("Enter your number:"))
+for i in range(1,n):
+    if i%3==0:
+        print(i)
+    
+#q14
+n=int(input("Enter your number:"))
+for i in range(1 ,n+1):
+    if i%2==0 and i%3==0:
+        print(f"{i} Divisible by both 2 and 3")
+                  
+#q15
+n=int(input("Enter your number:"))
+for i in range(0,n+1):
+    if i%2==0:
+        print(f"{i}:Even")
+
+#q16
+
+      
